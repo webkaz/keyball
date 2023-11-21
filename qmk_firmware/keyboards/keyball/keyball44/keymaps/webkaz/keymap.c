2af15920521374e690a96eed433db09c2214c316
webkaz <web.kaz@gmail.com>
2023-11-21 21:26:51 +0900

    update

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/webkaz/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/webkaz/keymap.c
@@ -62,22 +62,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 layer_state_t layer_state_set_user(layer_state_t state) {
     // Auto enable scroll mode when the highest layer is 3
-    keyball_set_scroll_mode(get_highest_layer(state) == 3);
+    //keyball_set_scroll_mode(get_highest_layer(state) == 3);
 
 //POINTING_DEVICE_AUTO_MOUSE_ENABLEが有効の時は以下有効となる
 #ifdef POINTING_DEVICE_AUTO_MOUSE_ENABLE
-    //レイヤー３のみAuto mouse layerを無効にする
+    //レイヤー３のみAuto mouse layerを無効にするt
     switch(get_highest_layer(remove_auto_mouse_layer(state, true))) {
-    case 3:
-        // Auto enable scroll mode when the highest layer is 3
-        // remove_auto_mouse_target must be called to adjust state *before* setting enable
-        state = remove_auto_mouse_layer(state, false);
-        set_auto_mouse_enable(false);
-        break;
-    default:
-        set_auto_mouse_enable(true);
-        break;
+        case 3:
+            // Auto enable scroll mode when the highest layer is 3
+            // remove_auto_mouse_target must be called to adjust state *before* setting enable
+            state = remove_auto_mouse_layer(state, false);
+            set_auto_mouse_enable(false);
+            keyball_set_scroll_mode(true);
+            break;
+        default:
+            set_auto_mouse_enable(true);
+            keyball_set_scroll_mode(false);
+            break;
     }
+#else
+    keyball_set_scroll_mode(get_highest_layer(state) == 3);
 #endif
 
     return state;
