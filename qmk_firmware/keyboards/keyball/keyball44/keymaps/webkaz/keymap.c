9db4ab2c58b45f69ff63187f22e48cec1ad7f4d9
webkaz <web.kaz@gmail.com>
2023-11-22 23:54:41 +0900

    update

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/webkaz/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/webkaz/keymap.c
@@ -20,6 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "quantum.h"
 
+#include "keyball_common.h"
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
