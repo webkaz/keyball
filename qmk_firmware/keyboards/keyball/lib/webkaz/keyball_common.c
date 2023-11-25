5bde701974e211ff761b54c4554d4b9649cb67da
web.kaz <web.kaz@gmail.com>
2023-11-25 15:47:49 +0000

    update

--- qmk_firmware/keyboards/keyball/lib/webkaz/keyball_common.c
+++ qmk_firmware/keyboards/keyball/lib/webkaz/keyball_common.c
@@ -6,7 +6,7 @@
 // 独自 スクロールレイヤーの規定値
 #define SCROLL_LAYER 3
 
-__attribute__((weak)) layer_state_t common_layer_state(layer_state_t state){
+layer_state_t common_layer_state(layer_state_t state){
 
 //POINTING_DEVICE_AUTO_MOUSE_ENABLEが有効の時は以下有効となる
 #ifdef POINTING_DEVICE_AUTO_MOUSE_ENABLE
